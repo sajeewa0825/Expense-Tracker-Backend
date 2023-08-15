d924470d2261175e8129ec55ac1fd91dc9a5fbbb
sajeewa0825 <sajeewa0825@gmail.com>
2023-08-16 02:06:19 +0530

    update user dashbord

--- module/user/controlers/userDashbord.js
+++ module/user/controlers/userDashbord.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 
 const dashbord = async (req, res) => {
     const userModel = mongoose.model('user');
+    const transactionModel = mongoose.model('transaction');
 
     const user = await userModel.findById(req.user._id).select('-password'); //select('-password') to not show password in response
 
+    const transaction = await transactionModel.find({
+        user_id:  req.user._id
+    }).sort("-createdAt").limit(3);
+
     res.status(200).json({
         status: 'success',
-        data:user
+        data:user,
+        transaction
     })
 };
 
