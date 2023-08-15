061889d4cfa53673ba9ca2688e3d0c1e1e5309b8
sajeewa0825 <sajeewa0825@gmail.com>
2023-08-16 01:43:09 +0530

    add get transaction

---
+++ module/transaction/controlers/gettransaction.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const gettransaction = async (req, res) => {
+
+    const transactionModel = mongoose.model('transaction');
+
+    const transaction = await transactionModel.find({
+        user_id:  req.user._id,
+        ...req.query // get end point additonal data ex- http://localhost:3000/api/transaction/gettransaction?transaction_type=expense
+    })
+
+    console.log(transaction)
+    res.status(200).json({
+        status: 'success',
+        data: transaction
+    })
+}
+
+module.exports = gettransaction;
--- module/transaction/transaction.routes.js
+++ module/transaction/transaction.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const middleware = require("../../middleware/auth");
 const addIncome = require("./controlers/addincome");
 const addexpenses = require("./controlers/addexpense");
+const gettransaction = require("./controlers/gettransaction");
 
 const transactionRoutes = express.Router() 
 
@@ -10,5 +11,6 @@ transactionRoutes.use(middleware)
 
 transactionRoutes.post("/addincome",addIncome)
 transactionRoutes.post("/expenses",addexpenses)
+transactionRoutes.get("/gettransaction",gettransaction)
 
 module.exports = transactionRoutes
