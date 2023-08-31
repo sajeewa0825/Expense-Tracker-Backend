a476a2a625d2355a42bc5ffd8acbef685e024ce0
sajeewa0825 <sajeewa0825@gmail.com>
2023-08-31 15:38:33 +0530

    add not found 404

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const cors = require("cors");
 const errorHandler = require('./handler/errorHandler');
 const userRoutes = require('./module/user/user.routes');
 const transactionRoutes = require('./module/transaction/transaction.routes');
 
 const app = express();
+app.use(cors())
 
 mongoose.connect(process.env.MONGO_URL,{}).then( () =>{
   console.log("connection succesfuly");
@@ -21,6 +23,13 @@ require("./model/transactionModel")
 app.use(express.json());
 app.use("/api/user",userRoutes)
 app.use("/api/transaction",transactionRoutes)
+
+app.all("*", (req, res, next) =>{
+  res.status(404).json({
+    status:"fail",
+    message:"404 not found"
+  })
+})
 app.use(errorHandler);
 
 app.listen(3000, () => {
