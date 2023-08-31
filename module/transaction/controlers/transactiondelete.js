b573b7f23b8d0e81e5ed53d0e3a3589b74228f8a
sajeewa0825 <sajeewa0825@gmail.com>
2023-08-31 14:38:37 +0530

    add transaction delete

---
+++ module/transaction/controlers/transactiondelete.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const validator = require("validator")
+
+
+const deleteTransaction = async (req, res) => {
+    const transactionModel = mongoose.model("transaction")
+    const userModel = mongoose.model("user");
+    const { t_id } = req.params
+    console.log(t_id)
+
+    const gettransaction = await transactionModel.findOne({
+        _id: t_id
+    })
+
+    if (!validator.isMongoId(t_id.toString())) throw "provide valid id"
+
+    if (!gettransaction) throw "trans action not found"
+
+    if (gettransaction.transaction_type == "income") {
+        await userModel.updateOne({
+            _id: gettransaction.user_id
+        }, {
+            $inc: {
+                balance: gettransaction.amount * -1,
+            }
+        },
+            {
+                runValidators: true
+            });
+    } else {
+        await userModel.updateOne({
+            _id: gettransaction.user_id
+        }, {
+            $inc: {
+                balance: gettransaction.amount,
+            }
+        },
+            {
+                runValidators: true
+            });
+    }
+
+    await transactionModel.deleteOne({
+        _id:t_id
+    })
+
+    res.status(200).json({
+        status: "succesfully",
+        message: "Transaction deleted"
+    })
+}
+
+module.exports = deleteTransaction;
--- module/transaction/transaction.routes.js
+++ module/transaction/transaction.routes.js
@@ -3,6 +3,7 @@ const middleware = require("../../middleware/auth");
 const addIncome = require("./controlers/addincome");
 const addexpenses = require("./controlers/addexpense");
 const gettransaction = require("./controlers/gettransaction");
+const deletedtransaction = require("./controlers/transactiondelete");
 
 const transactionRoutes = express.Router() 
 
@@ -12,5 +13,6 @@ transactionRoutes.use(middleware)
 transactionRoutes.post("/addincome",addIncome)
 transactionRoutes.post("/expenses",addexpenses)
 transactionRoutes.get("/gettransaction",gettransaction)
+transactionRoutes.delete("/:t_id",deletedtransaction)
 
 module.exports = transactionRoutes
